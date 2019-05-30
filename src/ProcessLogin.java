dacdd75ecae690ce377c30201b39f95e9d2fca64
16130296 <48484322+16130296@users.noreply.github.com>
2019-05-30 08:11:25 +0700

    comment trong code

--- src/ProcessLogin.java
+++ src/ProcessLogin.java
@@ -28,16 +28,25 @@ public class ProcessLogin extends HttpServlet {
 		request.setCharacterEncoding("utf8");
 		response.setCharacterEncoding("utf8");
 		RequestDispatcher dis = request.getRequestDispatcher("Login.jsp");
+
+		//2. get email, pass và sđt đến lớp ProcessLogin
+
 		String email = request.getParameter("name");
 		String pass = request.getParameter("password");
 		String sdt = request.getParameter("name");
-
+		
+		//kết thúc lấy dữ liệu
+		
+		//3. checkUserEmail()(hàm kiểm tra Email có trong database không) chuyển đến DaoSQL
+		
 		if (test.checkUserEmail(email, pass)) {
 			response.sendRedirect("index.html");
 		}
 		if (test.checkUserSdt(sdt, pass)) {
 			response.sendRedirect("index.html");
 		}
+		//5. Nếu b=true thì trả về trang index.html
+		// Nếu b = false thì trả về trang Login.jsp
 		else if(!test.checkUserEmail(email, pass)){
 			request.setAttribute("error", "thông tin đăng nhập sai");
 			dis.forward(request, response);
